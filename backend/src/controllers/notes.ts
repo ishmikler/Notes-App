b2a34f901c03914461e03b98add2727f26360ddc
Ivy <ivyameyer@gmail.com>
2023-11-14 19:33:42 -0700

    improved error handeling

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from 'express';
 import NoteModel from '../models/note';
+import createHttpError from 'http-errors';
+import mongoose from 'mongoose';
 
 //get all notes
 export const getNotes: RequestHandler = async (req, res, next) => {
@@ -15,22 +17,43 @@ export const getNotes: RequestHandler = async (req, res, next) => {
 //get single note
 export const getNote: RequestHandler = async (req, res, next) => {
   const noteId = req.params.noteId;
+
   try {
+    if (!mongoose.isValidObjectId(noteId)) {
+      throw createHttpError(400, 'Invalid node ID');
+    }
     const note = await NoteModel.findById(noteId).exec();
+    if (!note) {
+      throw createHttpError(404, 'Note not found');
+    }
     res.status(200).json(note);
   } catch (error) {
     next(error);
   }
 };
 
+interface CreateNoteBody {
+  title?: string;
+  text?: string;
+}
+
 //create note
-export const createNote: RequestHandler = async (req, res, next) => {
+export const createNote: RequestHandler<
+  unknown,
+  unknown,
+  CreateNoteBody,
+  unknown
+> = async (req, res, next) => {
   //send title and text in body of request
   //outside of try block b/c it wont throw an error
   const title = req.body.title;
   const text = req.body.text;
 
   try {
+    if (!title) {
+      throw createHttpError(400, 'Note must have a title');
+    }
+
     //use mongoose to create note and save in variable so you can send back to the client and update UI
     const newNote = await NoteModel.create({
       title: title,

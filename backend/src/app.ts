7a8178338b1d8676ebccaf8bb6f7f72a49b1c79d
Ivy <ivyameyer@gmail.com>
2025-03-10 21:26:02 -0600

    moved server code into proper file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,27 +1,23 @@
-import 'dotenv/config';
-import express, { NextFunction, Request, Response } from 'express';
-import notesRoutes from './routes/notes';
-import morgan from 'morgan';
-import createHttpError, { isHttpError } from 'http-errors';
-
-//create server
-const app = express();
+//import express, { NextFunction, Request, Response } from 'express';
+//import notesRoutes from './routes/notes';
+//import morgan from 'morgan';
+//import createHttpError, { isHttpError } from 'http-errors';
 
 //logs for endpoints
-app.use(morgan('dev'));
+//app.use(morgan('dev'));
 
 //tell express to accept json bodies
-app.use(express.json());
+//app.use(express.json());
 
 //middleware that catches request to this endpoint, and forwards to notesRoutes
-app.use('/api/notes', notesRoutes);
+//app.use('/api/notes', notesRoutes);
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
   next(createHttpError(404, 'Endpoint not found'));
-});
+});*/
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+/*app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
   console.error(error);
   let errorMessage = 'An unknown error occured';
   let statusCode = 500;
@@ -32,4 +28,4 @@ app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
   res.status(statusCode).json({ error: errorMessage });
 });
 
-export default app;
+export default app;*/
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,22 @@
-import app from './app';
-import env from './util/validateEnv';
-import mongoose from 'mongoose';
+import express from 'express';
 
-const port = env.PORT;
+//create server
+const app = express();
+
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World;');
+});
+
+app.listen(port, () => {
+  console.log('Sever listening on port: ' + port);
+});
 
 //connect mongoose to mongodb
 //process.env to access env variables
 //connect expects a string, returns a promise
-mongoose
+/*mongoose
   .connect(env.MONGO_CONNECTION_STRING)
   .then(() => {
     console.log('Mongoose connected');
@@ -15,4 +24,4 @@ mongoose
       console.log('Server listening on port ' + port);
     });
   })
-  .catch(console.error);
+  .catch(console.error);*/
